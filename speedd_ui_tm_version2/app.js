3c72178060d2883d9e055619dd7532d3fef917c9
Alexander Kofman <KOFMAN@il.ibm.com>
2016-02-23 19:17:26 +0200

    fix a couple of issues in TM UI

--- speedd_ui_tm_version2/app.js
+++ speedd_ui_tm_version2/app.js
@@ -48,10 +48,7 @@ app.get('/', function(req, res) {
 
 var ser = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
-  
-  setKafka();
   setSocket();
-  setConsumerEvents();
 });
 
 function setSocket(){
@@ -66,20 +63,8 @@ function setSocket(){
 		socket.emit('ramp-list', { rampLoc: rampLoc });
 		// sends a list of already identified events
 		socket.emit('event-list',{ eventList: JSON.stringify(eventList)});
-		
-		socket.on('my other event', function (data) {
-			console.log(data);
-		});
-		socket.on('speedd-out-events', function (data) {
-			console.log(data);
-			var toSend = [{ topic: 'speedd-traffic-admin', messages: data, partition: 0 }];
-			producer.send(toSend, function (err, data) {
-				console.log(toSend);
-				
-				// saves the admin ramp rate setting in event list
-				eventList.push(JSON.parse(toSend.messages));
-			});
-		});
+
+		setKafka();	
 	});
 }
 
@@ -92,37 +77,46 @@ function setKafka(){
 	consumer = new Consumer(
 		client, 
 		// payloads
-			[{ topic: 'speedd-traffic-actions', partition: 0, offset: 0 },
-			 { topic: 'speedd-traffic-out-events', partition: 0, offset: 0 }
+			[{ topic: 'speedd-traffic-actions'},
+			 { topic: 'speedd-traffic-out-events'}
 			 ],
 		// options
-		{fromOffset: false} // true = read messages from beginning
+		{
+			groupId: 'kafka-node-group',//consumer group id, default `kafka-node-group` 
+			// Auto commit config 
+			autoCommit: true,
+			autoCommitIntervalMs: 1000,
+			// The max wait time is the maximum amount of time in milliseconds to block waiting if insufficient data is available at the time the request is issued, default 100ms 
+			fetchMaxWaitMs: 100,
+			// This is the minimum number of bytes of messages that must be available to give a response, default 1 byte 
+			fetchMinBytes: 1,
+			// The maximum bytes to include in the message set for this partition. This helps bound the size of the response. 
+			fetchMaxBytes: 1024 * 10,
+			// If set true, consumer will fetch message from the given offset in the payloads 
+			fromOffset: false,
+			// If set to 'buffer', values will be returned as raw buffer objects. 
+			encoding: 'utf8'
+		}
 	);
 
-	//// Setting up Kafka Producer
-
-	Producer = kafka.Producer;
-	producer = new Producer(client);
+	//consumer.setOffset('speedd-traffic-out-events', 0, 0);
+	//consumer.setOffset('speedd-traffic-actions', 0, 0);
 	
-	producer.on('ready', function () {
-		producer.createTopics(['speedd-traffic-admin'], function (err, data) {
-			console.log(err);
-		});
-	});
+	setConsumerEvents();	
 }
 
 function setConsumerEvents(){
 
 	console.log("Setting up Consumer on-message event");
 	
-	consumer.on('ready', function () {
-		console.log("consumer listening");
-	});
 	consumer.on('error', function (err) {
-		console.log("Kafka Error: Consumer - " + err);
+		console.log("Kafka Error: Consumer: " + err);
+	});
+	consumer.on('offsetOutOfRange', function (err) {
+		console.log("Offset out of range: " + JSON.stringify(err));
 	});
 	consumer.on('message', function (message) {
-		console.log(message.value);
+		console.log("Got message:" + message.value);
 		io.emit('speedd-out-events', message.value);
 		
 		// checks if event is one that should be displayed in the ui
