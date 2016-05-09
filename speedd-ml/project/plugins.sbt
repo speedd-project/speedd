fdda489dbdb2b12937c7c18884887856a1838cab
Vagelis Michelioudakis <vagmcs@gmail.com>
2016-05-09 18:45:01 +0300

    speedd-ml old version removed

--- speedd-ml/project/plugins.sbt
+++
@@ -1,3 +0,0 @@
-addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.0.0")
-
-addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.5")
--- speedd-ml/src/main/scala/org/speedd/ml/util/logic/DerivedAtom.scala
+++
@@ -1,6 +0,0 @@
-package org.speedd.ml.util.logic
-
-import lomrf.logic.{AtomicFormula, Variable}
-
-class DerivedAtom(override val symbol: String, override val terms: Vector[Variable]) extends AtomicFormula(symbol, terms)
-
