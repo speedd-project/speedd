d3222be388ab18fa1873c26f92e6219f3d30049b
Vagelis Michelioudakis <vagmcs@gmail.com>
2016-06-27 17:19:34 +0300

    CNRS Simulation Data Loader WIP and inference using functions

---
+++ speedd-ml/src/main/scala/org/speedd/ml/loaders/cnrs/simulation/AnnotationDataLoader.scala
@@ -0,0 +1,8 @@
+package org.speedd.ml.loaders.cnrs.simulation
+
+/**
+  * Created by vagmcs on 24/6/2016.
+  */
+object AnnotationDataLoader {
+
+}
---
+++ speedd-ml/src/main/scala/org/speedd/ml/model/cnrs/simulation/Input.scala
@@ -0,0 +1,8 @@
+package org.speedd.ml.model.cnrs.simulation
+
+/**
+  * Created by vagmcs on 24/6/2016.
+  */
+class Input {
+
+}
